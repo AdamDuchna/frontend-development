1706964cd158f41947fd55f8f2e1144ce56e0d52
Adam <adamduchna43@gmail.com>
2021-12-22 08:50:19 +0100

    created actions

---
+++ sprawdzian1/ducks/catpackages/actions.js
@@ -0,0 +1,16 @@
+const CAT_ADD = 'CAT_ADD'
+const CAT_DEL = 'CAT_DEL'
+const CAT_EDIT = 'CAT_EDIT'
+
+export const addNoteAction = (payload) => ({
+    type: CAT_ADD,
+    payload
+});
+export const addNoteAction = (payload) => ({
+    type: CAT_DEL,
+    payload
+});
+export const addNoteAction = (payload) => ({
+    type: CAT_EDIT,
+    payload
+});
